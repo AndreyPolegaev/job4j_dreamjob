f88894b24bd73af2fb9e3caa60db0101ac1bec4f
AndreyPolegaev <79646307753@yandex.ru>
2021-11-12 23:53:15 +0300

    JSON формат.

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,22 +1,51 @@
 package ru.job4j.dream.servlet;
 
-import javax.servlet.ServletException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ru.job4j.dream.model.Email;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class GreetingServlet extends HttpServlet {
 
+    private static final Gson GSON = new GsonBuilder().create();
+
+    private final List<Email> emails = new CopyOnWriteArrayList<>();
+
+    /**
+     * ajax посылает данные типа json в doPost().
+     * дессиарилизуем идобавляем в лист.
+     * Обратно сериализуем и отправляем ответ типа json для вывода на сайт.
+     */
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
-            ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
-        PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
-        writer.flush();
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Email email = GSON.fromJson(req.getReader(), Email.class);
+        emails.add(email);
+        resp.setContentType("application/json; charset=utf-8");
+        OutputStream output = resp.getOutputStream();
+        String json = GSON.toJson(email);
+        output.write(json.getBytes(StandardCharsets.UTF_8));
+        output.flush();
+        output.close();
+    }
+
+    /**
+     * При обработке GET запроса происходит сериализация списка добавленных почтовых адресов.
+     * и отправка этого списка в виде json
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json; charset=utf-8");
+        OutputStream output = resp.getOutputStream();
+        String json = GSON.toJson(emails);
+        output.write(json.getBytes(StandardCharsets.UTF_8));
+        output.flush();
+        output.close();
     }
 }
